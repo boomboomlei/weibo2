58ca94a348234ed4612f754e85fb93c834463ef6
Your Nameboomboomlei <1846352100@qq.com>
2019-05-19 03:21:31 +0000

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+    <title>@yield('title','weibo app')-无敌的小老虎</title>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
---
+++ resources/views/static_pages/index.blade.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>weibo app</title>
+</head>
+<body>
+    <h1>index页</h1>
+</body>
+</html>
